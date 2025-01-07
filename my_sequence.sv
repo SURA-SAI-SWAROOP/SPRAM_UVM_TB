508ade7f9d13c3c14834cc069087c9851438009b
SURA SAI SWAROOP <128355949+SURA-SAI-SWAROOP@users.noreply.github.com>
2025-01-07 22:08:05 +0530

    my_sequence.sv

--- my_sequence.sv
+++ my_sequence.sv
@@ -1,15 +1,18 @@
 class my_sequence extends uvm_sequence#(my_transaction);
-`uvm_object_utils (my_sequence)
-function new(string name="my_sequence");
-super.new(name);
-endfunction
-task body();
-my_transaction req;
-req=my_transaction:: type_id::create("req");
-repeat (32) begin
-start_item(req);
-assert(req.randomize());
-finish_item(req);
-end
-endtask
+  `uvm_object_utils (my_sequence)
+  
+  function new(string name="my_sequence");
+    super.new(name);
+  endfunction
+    
+  task body();
+    my_transaction req;
+    req=my_transaction::type_id::create("req");
+    repeat (32) begin
+      start_item(req);
+      assert(req.randomize());
+      finish_item(req);
+    end
+      
+  endtask
 endclass
